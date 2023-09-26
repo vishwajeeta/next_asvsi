5c64af3c7525492d09dc8bba83cc802ef959e341
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-09-26 20:27:21 +0530

    increasing performance

--- app/window/page.tsx
+++ app/window/page.tsx
@@ -1,53 +1,88 @@
 import React from "react";
+import Image from "next/image";
 import './globals.css';
 function page(){
   return(
     <div>
       
-<body style={{background:"icons/wallpaper.webp"}}>
+<body style={{background:"/icons/pc.png"}}>
   <div id="windows">
     <div>
-      <img src="icons/pc.png" alt="" />
+      <Image src="/icons/pc.png" alt="no1" width={55} height={40} />
       <p>This pc</p>
     </div>
     <div>
-      <img src="icons/recycle.png" alt="" /><p>This pc</p>
+      <Image src="/icons/recycle.png" alt="omg" width={55} height={44}/>
+      
     </div>
-    <div> <img src="icons/folder.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/recycle.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/edge.png" alt="" /><p>This pc</p></div>
+    <div> <Image src="/icons/folder.png" alt="no3" width={55} height={40}/><p>This pc</p></div>
+    
+    <div> <Image src="/icons/edge.png" alt="no4" width={55} height={40}/><p>This pc</p></div>
 
-    <div> <img src="icons/pc.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/recycle.png" alt="" /><p>This pc</p></div>
+    <div> <Image src="/icons/pc.png" alt="no5" width={55} height={40}/><p>This pc</p></div>
+    <div>
+      <Image src="/icons/recycle.png" alt="omg" width={55} height={44}/>
+      
+    </div>
 
-    <div> <img src="icons/folder.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/recycle.png" alt="" /><p>This pc</p></div>
+    <div> <Image src="/icons/folder.png" alt="no6" width={55} height={40}/><p>This pc</p></div>
+    
+    <div>
+      <Image src="/icons/recycle.png" alt="omg" width={55} height={44}/>
+      
+    </div>
+
+    <div> <Image src="/icons/folder.png" alt="no7" width={55}  height={40}/><p>This pc</p></div>
+  
+
+    <div>
+      <Image src="/icons/recycle.png" alt="omg" width={55} height={44}/>
+      
+    </div>
+
+    <div> <Image src="/icons/pc.png" alt="no8" width={55} height={40}/><p>This pc</p></div>
+    
+
+    <div> <Image src="/icons/pc.png" alt="no9" width={55} height={40}/><p>This pc</p></div>
+    <div>
+      <Image src="/icons/recycle.png" alt="omg" width={55} height={44}/>
+      
+    </div>
+    <div>
+      <Image src="/icons/recycle.png" alt="omg" width={55} height={44}/>
+      
+    </div>
+    <div>
+      <Image src="/icons/recycle.png" alt="omg" width={55} height={44}/>
+      
+    </div>
+
+    <div> <Image src="/icons/folder.png" alt="no10" width={55} height={40}/><p>This pc</p></div>
+    
+    <div> <Image src="/icons/edge.png" alt="no11" width={55} height={40}/><p>This pc</p></div>
+
+    <div> <Image src="/icons/pc.png" alt="no12" width={55} height={40}/><p>This pc</p></div>
+    
+
+    <div> <Image src="/icons/folder.png" alt="no13" width={55} height={40}/><p>This pc</p></div>
+    
 
 
-    <div> <img src="icons/folder.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/recycle.png" alt="" /><p>This pc</p></div>
-
-
-
-    <div> <img src="icons/pc.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/recycle.png" alt="" /><p>This pc</p></div>
-
-    <div> <img src="icons/pc.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/recycle.png" alt="" /><p>This pc</p></div>
-
-    <div> <img src="icons/folder.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/recycle.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/edge.png" alt="" /><p>This pc</p></div>
-
-    <div> <img src="icons/pc.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/recycle.png" alt="" /><p>This pc</p></div>
-
-    <div> <img src="icons/folder.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/recycle.png" alt="" /><p>This pc</p></div>
-
-
-    <div> <img src="icons/folder.png" alt="" /><p>This pc</p></div>
-    <div> <img src="icons/recycle.png" alt="" /><p>This pc</p></div>
+    <div> <Image src="/icons/folder.png" alt="no14" width={55} height={40}/><p>This pc</p></div>
+    <div>
+      <Image src="/icons/recycle.png" alt="omg" width={55} height={44}/>
+      
+    </div><div>
+      <Image src="/icons/recycle.png" alt="omg" width={55} height={44}/>
+      
+    </div>
+    <div>
+      <Image src="/icons/recycle.png" alt="omg" width={55} height={44}/>
+      
+    </div><div>
+      <Image src="/icons/recycle.png" alt="omg" width={55} height={44}/>
+      
+    </div>
 
   </div>
   </body>
