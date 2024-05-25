ecfb3a7ebb9f8ec0768e827428e5cac6a565e31d
vishwajeeta <gs699542@gmail.com>
2024-05-25 21:51:03 +0530

    adding outer function call for useState

--- app/practice/page.jsx
+++ app/practice/page.jsx
@@ -3,12 +3,23 @@ import React, { useState } from 'react'
 
 function page() {
     const [number,setNumber]=useState(0)
+    const increase =()=>{
+      let num=incre()
+      setNumber(num)
+    }
   return (
     <div>
-      <h1>number={number}</h1>
-      <button onClick={()=>setNumber(number+1)}>click Me</button>
+      <h1 id='data'>{number}</h1>
+      <button onClick={()=>setNumber(number+1)} title='inner Function call'>click Me</button>
+      <button onClick={increase} title='outer function call'>click Me!</button>
     </div>
   )
+}
+let incre =()=>{
+  let omg=document.getElementById("data").outerText
+  omg=parseInt(omg)
+  omg++
+  return omg
 }
 
 
