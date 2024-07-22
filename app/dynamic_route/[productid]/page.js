c9b01a69b3cfaf414b474a25187efb55d0aac9e2
vishwajeeta <gs699542@gmail.com>
2024-07-22 06:56:21 +0530

    sucessfully learned dynamic routing

---
+++ app/dynamic_route/[productid]/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page({params}) {
+  return (
+    <div>
+      hii:{params.productid}
+    </div>
+  )
+}
+
+export default page
+
+//3000/dynamic_route/anything
---
+++ app/dynamic_route/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      this is dynamic sample page
+    </div>
+  )
+}
+
+export default page
